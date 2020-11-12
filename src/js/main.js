4c649f53003a296039d5066bf3791a73b6cb7882
Vasyl Tsiutsyk <v.tsyutsyk@gmail.com>
2020-11-12 13:19:17 +0200

    add clients slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-console.log('main');
+const mySwiper = new Swiper('.swiper-container', {
+
+    direction: 'horizontal',
+    slidesPerView: 5,
+    grabCursor: true,
+    slidesOffsetBefore: '50',
+
+    navigation: {
+        nextEl: '.swiper-btn-next',
+        prevEl: '.swiper-btn-prev',
+    }
+})
